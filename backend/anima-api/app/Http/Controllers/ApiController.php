ba06866ea70ca095742ade88ae41ec12ed3ba385
Kevin Mora Pais <kevinmorapais532@gmail.com>
2021-07-24 20:37:59 -0300

    Deleted obsolete class

--- backend/anima-api/app/Http/Controllers/ApiController.php
+++ backend/anima-api/app/Http/Controllers/ApiController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class ApiController extends Controller
+class ApiController
 {
     public function sendResponse($result, $message)
     {
