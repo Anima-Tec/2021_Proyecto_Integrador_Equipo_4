4da952366993e9d58554e17568123c658a6fcd1e
JhonAires-Anima <--global>
2021-08-30 20:45:54 -0300

    react router template added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import classes from './App.module.scss';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className={classes.App}>
-      <header className={classes['App-header']}>
-        <img src={logo} className={classes['App-logo']} alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={classes['App-link']}
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path='/' exact>
+          <p>home page</p>
+        </Route>
+        <Route path='/react'>
+          <div className={classes.App}>
+            <header className={classes['App-header']}>
+              <img src={logo} className={classes['App-logo']} alt='logo' />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className={classes['App-link']}
+                href='https://reactjs.org'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Route>
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
