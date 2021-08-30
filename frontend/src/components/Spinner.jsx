83a1196e171abf5377bb9d55525681a1f1d2f87b
Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>
2021-08-30 08:34:26 -0300

    Add-Spinner

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+    const LoadingSpinner = () => (
+      <div>
+      <div className = "spinner">
+        </div>
+        <div id="carga"></div>
+      </div>
+    );
+
+export default LoadingSpinner;
