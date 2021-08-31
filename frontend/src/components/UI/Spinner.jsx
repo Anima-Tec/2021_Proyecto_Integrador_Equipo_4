ec66b4f98912ff9ac1a2e28b167c4fd4dbd76e0b
Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>
2021-08-31 10:01:15 -0300

    Fix pr

--- frontend/src/components/UI/Spinner.jsx
+++ frontend/src/components/UI/Spinner.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import classes from "./Spinner.module.scss";
 
 const Spinner = () => (
-  <div>
+  <>
     <div className={classes.spinner}></div>
     <div className={classes.carga}></div>
-  </div>
+  </>
 );
 
 export default Spinner;
