726de1f9f4c55676a07d8c90ac9ae3776205bffd
Agustin-1006 <56725338+Agustin-1006@users.noreply.github.com>
2021-08-31 11:35:26 -0300

    Add-Spinner (#89)
    * Add-Spinner
    
    * fix pr Spinner
    
    * fix pr
    
    * Fix pr
    
    * fix pr
    
    Co-authored-by: Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>

---
+++ frontend/src/components/UI/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Spinner.module.scss";
+
+const Spinner = () => (
+  <>
+    <div className={classes.spinner}></div>
+    <div className={classes.carga}></div>
+  </>
+);
+
+export default Spinner;
