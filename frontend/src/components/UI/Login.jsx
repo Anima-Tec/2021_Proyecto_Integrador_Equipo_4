16f2944ab24c4e428428038e6d3d9a8637ec3628
Alex Torres <alex.torres@anima.edu.uy>
2021-09-02 09:19:31 -0300

    Ya leí los comments y cambié lo que pedías

--- frontend/src/components/UI/Login.jsx
+++ frontend/src/components/UI/Login.jsx
@@ -2,25 +2,33 @@ import classes from "./Login.module.scss";
 import React from "react";
 import Popup from "reactjs-popup";
 import "../../../node_modules/reactjs-popup/dist/index.css";
+
+const LoginHandler = (event) => {
+  event.preventDefault();
+}
+
 const Login = () => (
-  <Popup trigger={<button className={classes.LoginBtn}>Acceder</button>} modal>
-    <div className={classes.modal}>
-      <div className={classes.WelcomeContainer}>
-        <p>Bienvenido</p>
+  <Popup
+    trigger={<button className={classes["login-btn"]}>Acceder</button>}
+    modal
+  >
+    <div className={['login-container']}>
+      <div className={classes["welcome-container"]}>
+        <h4>Bienvenido</h4>
       </div>
-      <div className={classes.InfoContainer}>
-        <input placeholder="Email"></input>
-        <input placeholder="Contraseña"></input>
+      <form onSumbit={(LoginHandler)} className={classes["info-container"]}>
+        <input type='email' placeholder="Email"></input>
+        <input type='password' placeholder="Contraseña"></input>
         <a
           href="https://www.google.com/?hl=es"
           target="_blank"
           rel="noreferrer"
-          className={classes.ResetPssw}
+          className={classes["reset-passw"]}
         >
           Olvidé mi contraseña
         </a>
-        <button>Entrar</button>
-      </div>
+        <button type='sumbit'>Entrar</button>
+      </form>
     </div>
   </Popup>
 );
