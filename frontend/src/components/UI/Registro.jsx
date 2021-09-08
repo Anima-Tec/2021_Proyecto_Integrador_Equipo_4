5b5e89b0557d8d805f8ee6d1887463be01c541ec
unknown <katherine.damires@anima.edu.uy>
2021-09-08 16:39:06 -0300

    Cambios de pr sugeridos implementados pero a la espera de cambios en figma

--- frontend/src/components/UI/Registro.jsx
+++ frontend/src/components/UI/Registro.jsx
@@ -6,35 +6,30 @@ import classes from './Registro.module.scss';
 import '../../../node_modules/reactjs-popup/dist/index.css';
 
 const Registro = () => {
-  /*const [nombre, setNombre] = usestate('');
-  const [apellido, setApellido] = usestate('');
-  const [mail, setMail] = usestate('');
-  const [contrasenia, setContrasenia] = usestate('');
-  const [confirmarCotrasenia, setConfirmarContrasenia] = usestate('');*/
 
   const handlesubmit = () => {
     e.preventdefault();
   };
 
   return (
-    <Popup trigger={<button className={classes.registroButton}>Registrarse</button>}
+    <Popup trigger={<button className={classes.registerButton}>Registrarse</button>}
       modal
     >
       <div className={classes.modal}>
-        <div className={classes.registroHeader}>
-          <p>Registrate para continuar</p>
+        <div className={classes.registerHeader}>
+          <p>¡Registrate!</p>
         </div>
-        <div className={classes.registroBody}>
+        <div className={classes.registerBody}>
           <form onSubmit={(handlesubmit)}>
-            <input type="text" placeholder="Nombre" id="nombre" value onChange={(e) => setNombre(e.target.vaule)} />
+            <input type="text" placeholder="Nombre" id="name"/>
 
-            <input type="text" placeholder="Apellido" id="Apellido" value onChange={(e) => setApellido(e.target.vaule)} />
+            <input type="text" placeholder="Apellido" id="surname"/>
 
-            <input type="text" placeholder="Mail" id="mail" value onChange={(e) => setMail(e.target.vaule)} />
+            <input type="email" placeholder="Mail" id="mail"/>
 
-            <input type="password" placeholder="Contraseña" id="contrasenia" value onChange={(e) => setContrasenia(e.target.vaule)} />
+            <input type="password" placeholder="Contraseña" id="password" minLength="8"/>
 
-            <input type="password" placeholder="Confirmar Contraseña" id="confirmarCotrasenia" value onChange={(e) => setConfirmarContrasenia(e.target.vaule)} />
+            <input type="password" placeholder="Confirmar Contraseña" id="confmirmPassword" minLength="8"/>
 
             <button type="submit">Registrarse</button>
           </form>
