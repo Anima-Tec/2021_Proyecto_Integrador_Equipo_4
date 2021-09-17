54b198af66b6e972031e451cd12a3781775bc3ad
Kevin Mora Pais <kevinmorapais532@gmail.com>
2021-09-16 22:57:47 -0300

    Changed account activation method

--- backend/anima-api/routes/api.php
+++ backend/anima-api/routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::put('/activate', 'App\Http\Controllers\ControllerU@accountActivationHandler');
+Route::post('/activate', 'App\Http\Controllers\ControllerU@accountActivationHandler');
 Route::post('/ollas/save', 'App\Http\Controllers\ControllerO@createOlla');
 Route::get('/ollas', 'App\Http\Controllers\ControllerO@getAll');
 Route::post('/register', 'App\Http\Controllers\ControllerU@tempRegister');
