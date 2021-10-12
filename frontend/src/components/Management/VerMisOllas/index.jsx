d65848d85ae7083fcee983f6bfea5aa137f4f74d
Katherine Damires Gonzalez <katherine.damires@globant.com>
2021-10-12 12:03:16 -0300

    changes

--- frontend/src/components/Management/VerMisOllas/index.jsx
+++ frontend/src/components/Management/VerMisOllas/index.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { ArrowForwardIos as ArrowForwardIosIcon } from '@material-ui/icons/';
+import React from "react";
+import { ArrowForwardIos as ArrowForwardIosIcon } from "@material-ui/icons/";
 
-import classes from './VerMisOllas.module.scss';
-import imgPrueba from '../../../assets/images/img-prueba.jpg';
+import classes from "./VerMisOllas.module.scss";
+import imgPrueba from "../../../assets/images/img-prueba.jpg";
 
 const VerMisOllas = () => {
-  
-
   return (
     <div className={classes.container}>
-      <div className={classes['container-content']}>
+      <div className={classes["container-content"]}>
         <h1 className={classes.title}>Benito camelo</h1>
         <button className={classes.state}>Olla sin necesidad</button>
         <p className={classes.description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius modi
-          tenetur sequi voluptate expedita quas aperiam. Modi fuga minus totam!
-          Ut quo ipsam unde atque nostrum explicabo sapiente maiores temporibus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius modi
-          tenetur sequi voluptate expedita quas aperiam. Modi fuga minus totam!
-          Ut quo ipsam unde atque nostrum explicabo sapiente maiores temporibus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius modi
-          tenetur sequi voluptate expedita quas aperiam. Modi fuga minus totam!
-          Ut quo ipsam unde atque nostrum explicabo sapiente maiores temporibus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius modi
-          tenetur sequi voluptate expedita quas aperiam. Modi fuga minus totam!
-          Ut quo ipsam unde atque nostrum explicabo sapiente maiores temporibus?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius modi
-          tenetur sequi voluptate expedita quas aperiam. Modi fuga minus totam!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          aliquam obcaecati tempora cum? Amet, facilis exercitationem, minima
+          hic, ipsam culpa officiis possimus iure reiciendis quo sunt. Molestias
+          nemo repudiandae voluptates. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Temporibus porro expedita distinctio ipsum autem,
+          exercitationem mollitia incidunt molestiae at dolorum repudiandae.
+          Distinctio in atque eligendi sunt ab dolor est quae! Lorem, ipsum
+          dolor sit amet consectetur adipisicing elit. Doloribus blanditiis odio
+          ipsam id vel dolorem nisi pariatur deleniti, nostrum iste, quas atque
+          cum quia delectus earum expedita quibusdam sunt voluptatibus. Lorem
+          ipsum dolor sit, amet consectetur adipisicing elit. Fugiat dicta
+          labore atque, autem dolores assumenda debitis adipisci quos minima
+          laborum quidem incidunt cupiditate inventore? Quibusdam quo obcaecati
+          nihil sed vitae!
         </p>
         <p className={classes.time}>Horario: 10:00 - 01:00</p>
-        <button className={classes['edit-button']}>Editar olla popular <ArrowForwardIosIcon/> </button>
+        <button className={classes["edit-button"]}>
+          Editar olla popular <ArrowForwardIosIcon />{" "}
+        </button>
       </div>
 
-      <div className={classes['container-img']}>
-        <img className={classes.img} src={imgPrueba} alt='img-prueba' />
+      <div className={classes["container-img"]}>
+        <img className={classes.img} src={imgPrueba} alt="img-prueba" />
       </div>
     </div>
   );
