bf4593210f7a80e39272c0a4bf16c2e5aabba2a3
Kevin Mora Pais <kevinmorapais532@gmail.com>
2021-11-01 12:38:40 -0300

    Added comment model

---
+++ backend/anima-api/database/migrations/2016_11_01_152334_comments.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('potID')->unsigned();
+            $table->foreign('potID')->references('id')->on('pots');
+            $table->bigInteger('userID')->unsigned();
+            $table->foreign('userID')->references('id')->on('users');
+            $table->datetime('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
