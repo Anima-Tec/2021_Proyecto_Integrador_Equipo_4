320ae27f6d65459c726c324c5011587d4598a94a
Kevin Mora Pais <kevinmorapais532@gmail.com>
2021-11-01 15:58:12 -0300

    Added full pot update support

--- backend/anima-api/app/Http/Controllers/ServiceHandler.php
+++ backend/anima-api/app/Http/Controllers/ServiceHandler.php
@@ -6,15 +6,59 @@ use Validator;
 use Illuminate\Http\Request;
 use App\Models\Pot;
 use App\Models\Donation;
-use Illuminate\Support\Facades\Cache;
 use App\Mail\Mailer;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\File;
 
 date_default_timezone_set("America/Argentina/Buenos_Aires");
 
 class ServiceHandler extends Controller
 {
     // Pots ------------------------------------------------------------------------------------------------------------------------------
+    public function updatePot(Request $request, $potID)
+    {
+        $dataValidation = Validator::make($request->all(), [
+            'name' => 'string|max:255',
+            'desc' => 'string',
+            'openFrom' => 'date_format:H:i',
+            'to' => 'date_format:H:i',
+            'address' => 'string',
+            'image' => 'mimes:jpg,png,jpeg,gif,svg',
+            'isInNeed' => [Rule::in(['1', '0'])],
+            'state' => [Rule::in(['1', '0'])]
+        ]);
+
+        if ($dataValidation->fails()) {
+            return response()->json([
+                'message' => 'Invalid values were provided, check documentation for validation requirements.',
+            ], 400);
+        }
+
+        $validatedData = $request->only(['name', 'desc', 'openFrom', 'to', 'address', 'image', 'isInNeed', 'state']);
+        $user = $request->user();
+        if (Pot::where('id', $potID)->doesntExist()) {
+            return response()->json([
+                'message' => 'Pot not found.'
+            ], 404);
+        }
+        $oldPot = Pot::where('id', $potID)->get()[0];
+        $newPot = ['name' => $validatedData['name'] ?? $oldPot->name, 'authorEmail' => $user->email, 'desc' => $validatedData['desc'] ?? $oldPot->desc, 'openFrom' => $validatedData['openFrom'] ??  $oldPot->openFrom, 'to' => $validatedData['to'] ?? $oldPot->to, 'address' => $validatedData['address'] ?? $oldPot->address, 'isInNeed' => $validatedData['isInNeed'] ?? $oldPot->isInNeed, 'state' => $validatedData['state'] ?? $oldPot->state];
+        if ($request->hasFile('image')) {
+            File::delete(public_path("/assets/pots/pot_$potID/pot_$potID.jpg"));
+            $request->file('image')->move(public_path("/assets/pots/pot_$potID"), "pot_$potID.jpg");
+        }
+        Pot::where('id', $potID)->update($newPot);
+        return response()->json([
+            'message' => 'Update successful.',
+            'newValues' => $newPot
+        ], 200);
+
+
+        $fileName = "pot_" . $potID . ".jpg";
+        $request->file('image')->move(public_path("/assets/pots/pot_$potID"), $fileName);
+        Pot::where('id', $potID)->update(['imageURL' => url("/assets/pots/pot_" . '/' . $fileName)]);
+    }
     public function getAllPotsFromUser(Request $request)
     {
         $user = $request->user();
@@ -29,19 +73,10 @@ class ServiceHandler extends Controller
 
     public function getAllPots()
     {
-        $potsCache = Cache::get('pots');
-
-        if ($potsCache) {
-            return response()->json([
-                'Pots' => $potsCache
-            ], 200);
-        }
 
         $Pots = Pot::where('state', 1)
             ->select('*')
             ->get();
-
-        Cache::put('pots', $Pots, 600);
 
         return response()->json([
             'Pots' => $Pots
@@ -89,8 +124,6 @@ class ServiceHandler extends Controller
         $fileName = "pot_" . $latestPot . ".jpg";
         $request->file('image')->move(public_path("/assets/pots/pot_$latestPot"), $fileName);
         Pot::where('id', $latestPot)->update(['imageURL' => url("/assets/pots/pot_$latestPot" . '/' . $fileName)]);
-
-        Cache::forget('pots');
 
         return response()->json([
             'message' => 'New pot created.'
